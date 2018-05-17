97bd7041979db7823a0770c1112d65ad69a5ae59
Madeline McFadden <mcfaddem@iu.edu>
2018-05-17 12:10:34 -0400

    new

---
+++ index.js
@@ -0,0 +1,47 @@
+const app = {
+    init(selectors) {
+      this.flicks = []
+      this.max = 0
+      this.list = document.querySelector(selectors.listSelector)
+      this.template = document.querySelector(selectors.templateSelector)
+  
+      document
+        .querySelector(selectors.formSelector)
+        .addEventListener('submit', ev => {
+          ev.preventDefault()
+          this.handleSubmit(ev)
+        })
+    },
+  
+    renderListItem(flick) {
+      const item = this.template.cloneNode(true)
+      item.classList.remove('template')
+      item.dataset.id = flick.id
+      item
+        .querySelector('.flickName')
+        .textContent = flick.name
+  
+      return item
+    },
+  
+    handleSubmit(ev) {
+      const f = ev.target
+      const flick = {
+        id: ++this.max,
+        name: f.flickName.value,
+      }
+  
+      this.flicks.unshift(flick)
+  
+      const item = this.renderListItem(flick)
+      this.list.insertBefore(item, this.list.firstElementChild)
+  
+      f.reset()
+    },
+  }
+  
+  app.init({
+    formSelector: '#flickForm',
+    listSelector: '#flickList',
+    templateSelector: '.flick.template',
+  })
